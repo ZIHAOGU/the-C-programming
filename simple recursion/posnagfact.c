17f8a7ce2ac0e8263064084c01f63bba425510db
ZIHAO GU <42879044+ZIHAOGU@users.noreply.github.com>
2019-02-19 00:53:35 -0500

    this is for simple recursion with if statement

---
+++ simple recursion/posnagfact.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int Factorial(const int N);
+int main(){
+	int N,NF;
+	scanf("%d",&N);
+	NF=Factorial(N);
+	if(NF)printf("%d!=%d\n",N,NF);
+	else printf("Invaild input\n");
+	
+	return 0;
+}
+int Factorial(const int N){
+	if(N==0||N==1){
+			return 1;
+	}
+			
+	else{
+		if(N>1){
+		return N*Factorial(N-1);	
+		}else if(N<0){
+			return 0;
+		} 
+		
+		
+	}
+}
